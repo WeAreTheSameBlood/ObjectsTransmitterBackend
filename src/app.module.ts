26ca57e984c9c2ec5fd5858cbc8be2237939c291
Andrii Hlybchenko <hlybchenko.andrii@gmail.com>
2025-05-10 03:13:32 +0300

    upd orm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ModelsModule } from '@src/modules/models/models.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from '@modules/users/users.module';
 import { AuthModule } from '@modules/auth/auth.module';
 import { AppWriteModule } from '@common/services/appwrite/appwrite.module';
@@ -13,15 +13,24 @@ import { HealthModule } from './modules/health/health.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type:     'postgres',
-      host:     process.env.POSTGRES_HOST!,
-      port:     process.env.POSTGRES_PORT! as unknown as number,
-      username: process.env.POSTGRES_USER!,
-      password: process.env.POSTGRES_PASSWORD!,
-      database: process.env.POSTGRES_DATABASE!,
-      autoLoadEntities: true,
-      synchronize: true,
+    // TypeOrmModule.forRoot({
+    //   type:     'postgres',
+    //   host:     process.env.POSTGRES_HOST!,
+    //   port:     process.env.POSTGRES_PORT! as unknown as number,
+    //   username: process.env.POSTGRES_USER!,
+    //   password: process.env.POSTGRES_PASSWORD!,
+    //   database: process.env.POSTGRES_DATABASE!,
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     ModelsModule,
     UsersModule,
